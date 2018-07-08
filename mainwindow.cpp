569b74574d469c3f73b072fc622be465b2d9779c
Grindewald23 <38288808+Grindewald23@users.noreply.github.com>
2018-07-08 18:56:44 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,49 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    /*----------------------tab GENERAL---------------------------------- */
+
+    //add a toolbar in the top of gridlayout
+    QToolBar *tabGeneralToolBar = new QToolBar();
+    tabGeneralToolBar->setStyleSheet("QToolBar{background-color: #ABABAB;}");
+
+    //create a group action to regroup action tools
+    QActionGroup *toolsGroup = new QActionGroup(this);
+    toolsGroup->addAction("Working tool");
+    toolsGroup->addAction(ui->actionSelection);
+    toolsGroup->addAction(ui->actionPen);
+    toolsGroup->addAction(ui->actionRectangle);
+    tabGeneralToolBar->addActions(toolsGroup->actions());
+
+    tabGeneralToolBar->addSeparator();
+
+    //create a group action to regroup action layers
+    QActionGroup *layersGroup = new QActionGroup(this);
+    layersGroup->addAction("Working layer");
+    layersGroup->addAction(ui->actionLow_layer_1);
+    layersGroup->addAction(ui->actionLow_layer_2);
+    layersGroup->addAction(ui->actionHigh_layer);
+    layersGroup->addAction(ui->actionEvents_layer);
+    layersGroup->addAction(ui->actionQuick_go_admin);
+    layersGroup->addAction(ui->actionStarting_point);
+    layersGroup->addAction(ui->actionRespawn_point);
+    tabGeneralToolBar->addActions(layersGroup->actions());
+
+    tabGeneralToolBar->addSeparator();
+
+    //create a group action to regroup action launch test
+    QActionGroup *launchTestGroup = new QActionGroup(this);
+    launchTestGroup->setExclusive(false);
+    launchTestGroup->addAction("Launch test settings");
+    launchTestGroup->addAction(ui->actionLaunch_test);
+    launchTestGroup->addAction(ui->actionActivate_fullscreen);
+    launchTestGroup->addAction(ui->actionActivate_music);
+    launchTestGroup->addAction(ui->actionActivate_character_setting);
+    tabGeneralToolBar->addActions(launchTestGroup->actions());
+
+
+    ui->tabGeneralGridLayout->setMenuBar(tabGeneralToolBar);
 }
 
 MainWindow::~MainWindow()
