2902bb5bfca07fae78c0ea7592dd640daf74b7e1
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-06-19 19:22:46 +0200

    Fix map loading

--- src/editormap.cpp
+++ src/editormap.cpp
@@ -23,10 +23,10 @@ void EditorMap::reset(std::uint16_t width, std::uint16_t height) {
     m_blockingLayer.resize(m_width * 2 * m_height * 2 * sizeof(std::uint8_t));
 
     std::pair<std::int8_t, std::int8_t> reset(-1, -1);
-    m_firstLayer.resize(m_width * m_height * 2 * sizeof(std::int8_t), reset);
-    m_secondLayer.resize(m_width * m_height * 2 * sizeof(std::int8_t), reset);
-    m_thirdLayer.resize(m_width * m_height * 2 * sizeof(std::int8_t), reset);
-    m_fourthLayer.resize(m_width * m_height * 2 * sizeof(std::int8_t), reset);
+    m_firstLayer.resize(m_width * m_height, reset);
+    m_secondLayer.resize(m_width * m_height, reset);
+    m_thirdLayer.resize(m_width * m_height, reset);
+    m_fourthLayer.resize(m_width * m_height, reset);
 }
 
 void EditorMap::save() {
