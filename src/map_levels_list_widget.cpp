fb9a913778f4a36b02e7d1e1aea12c81505f053f
GeoffreyÂ ROYER <geoffrey.royer@gmail.com>
2019-06-25 13:56:40 +0200

    Remove unused files

--- src/map_levels_list_widget.cpp
+++
@@ -1,9 +0,0 @@
-#include "map_levels_list_widget.hpp"
-#include "ui_map_levels_list_widget.h"
-
-MapLevelsListWidget::MapLevelsListWidget(QWidget* parent)
-    : QDockWidget(parent),
-      ui(new Ui::MapLevelsListWidget)
-{
-    ui->setupUi(this);
-}
