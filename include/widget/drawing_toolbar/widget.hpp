d0c7cd1606f2f0fb1e2fa8d71caf0da08bf96a72
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-07-07 11:18:16 +0200

    Start adding drawing toolbar

--- include/widget/drawing_toolbar/widget.hpp
+++ include/widget/drawing_toolbar/widget.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <QWidget>
+
 namespace Widget {
 namespace DrawingToolbar {
+
+class Widget : public ::QWidget {
+    Q_OBJECT
+};
 
 } // namespace DrawingToolbar
 } // namespace Widget
